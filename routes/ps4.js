835cc6260559dff7b170348c1fb18aa140d1e582
Gonzalo Rosales <gonzalorosales13@gmail.com>
2020-10-30 19:07:22 -0400

    cleaning up code and removed unnecessary comments

--- routes/ps4.js
+++ routes/ps4.js
@@ -9,9 +9,6 @@ router.route('/')
         res.render('index', {weather: 'no city has been passed yet'})
     })
     .post((req, res, next) => {
-        // let city = req.body.city;
-        // let key = weatherApiConfig.js.ps4Options.key;
-        // let url = weatherApiConfig.js.ps4Options.url + key + '&q= ' + city;
         handleApiCall(req)
             .then(value => {
                 // console.log(value);
@@ -36,27 +33,6 @@ const handleApiCall = (args) => {
         })
     })
 }
-
-// // Grabs name from HTML form and returns json object of result
-// router.post('/', async (request, response) => {
-//     if (typeof request.body.city === 'undefined') {
-//         // The parameter is missing, example response...
-//         res.status(400).json({error: 'missing parameter team', data: null}); // Only an  example
-//         return;
-//     }
-//
-//     //build the request URL using the input city and key from our weatherApiConfig.js file
-//     //'uri' : 'http://api.weatherapi.com/v1/current.json' + '?key=' + req.query.key + '&q= ' + req.query.q
-//     let city = request.body.city;
-//     let key = weatherApiConfig.js.ps4Options.key;
-//     let url = weatherApiConfig.js.ps4Options.url + key + '&q= ' + city;
-//     console.log(api_url);
-//
-//     let fetch_response = await fetch(url);
-//     let weatherData = await fetch_response.json();
-//     console.log(weatherData)
-//     response.json(weatherData);
-// });
 
 
 module.exports = router;
